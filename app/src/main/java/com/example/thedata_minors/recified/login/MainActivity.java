6c49a233c9557cd480a9ab8f36b3fbc69a1c65f4
TheAhBooBoo <yonni.luu@gmail.com>
2016-11-06 00:33:20 -0700

    Added PresenterAndView interface

--- app/src/main/java/com/example/thedata_minors/recified/login/MainActivity.java
+++ app/src/main/java/com/example/thedata_minors/recified/login/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.thedata_minors.recified;
+package com.example.thedata_minors.recified.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.thedata_minors.recified.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
